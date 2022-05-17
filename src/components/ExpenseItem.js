5de20933fc2a9bb42ae56b322b4abaf20d10fe0b
Gadius <gadlapaz@hotmail.com>
2022-05-17 13:58:57 -0600

    changing Expenses function to consts using arrows

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,6 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
 
     const month = props.date.toLocaleString('en-US',{month: 'long'});
     const day = props.date.toLocaleString('en-US',{day: '2-digit'});
