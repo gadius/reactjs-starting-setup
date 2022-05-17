469939bdbdf7add94433448f2b9da7324de06b13
Gadius <gadlapaz@hotmail.com>
2022-05-17 14:16:25 -0600

    click example on ExpenseItem using a const

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,22 @@
-import './ExpenseItem.css'
-import ExpenseDate from './ExpenseDate';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 const ExpenseItem = (props) => {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
-    const month = props.date.toLocaleString('en-US',{month: 'long'});
-    const day = props.date.toLocaleString('en-US',{day: '2-digit'});
-    const year = props.date.getFullYear();
-
-    return (
-    <div className='expense-item'>
-        <ExpenseDate date={props.date}/>
-        <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>{props.amount}</div>
-        </div>      
-    </div>
-    );
+const clickHandler = () => {
+    console.log("Click");
 }
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </div>
+  );
+};
 export default ExpenseItem;
