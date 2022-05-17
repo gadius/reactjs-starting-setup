84e01699ad98c82e920c38a6d93070c93d3f8827
Gadius <gadlapaz@hotmail.com>
2022-05-17 14:40:11 -0600

    Ejemplo de como ligar un evento + useState hook para modificar titulo

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,26 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+
 const ExpenseItem = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
 
-const clickHandler = () => {
+  const [title, setTitle] = useState(props.title); //siempre regresa un arreglo de 2 elementos, variable y la funcion que hace el update de la variable
+  //title = nombre variable, setTitle una funcion que se puede usar despues
+  //useState hook, para poder manipular el estatus de variables y poder renderizar denuevo el componente en la vista, las funciones normales no sirven de esa manera.
+  //al crear useState se crean "variables especiales" para poder usarlas dentro del componente
+
+  const clickHandler = () => {
+    setTitle('Updated Title folowing example');
     console.log("Click");
-}
+  };
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
